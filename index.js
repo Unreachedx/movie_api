d6365551b6bc292c67040d9b2cc04aaa31951df6
Patrick Oliphant <oliphant.p@web.de>
2024-05-18 02:47:34 +0200

    Updated index.js and passport.js

--- index.js
+++ index.js
@@ -142,6 +142,30 @@ app.put('/users/:Username', async (req, res) => {
   }
 });
 
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  // CONDITION TO CHECK ADDED HERE
+  if(req.user.Username !== req.params.Username){
+      return res.status(400).send('Permission denied');
+  }
+  // CONDITION ENDS
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+      $set:
+      {
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+      }
+  },
+      { new: true }) // This line makes sure that the updated document is returned
+      .then((updatedUser) => {
+          res.json(updatedUser);
+      })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).send('Error: ' + err);
+      })
+});
 
 // Allow users to add a movie to their list of favorites
 app.post('/users/:Username/favorites/:movieID', async (req, res) => {
