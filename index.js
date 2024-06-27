0c9881c6aebbf1dbf7c84758d60c5c0b3104c813
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-06-27 16:26:28 +0200

    updated index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const passport = require('passport');
 require('./passport');
 
 // Define allowed origins for CORS
-let allowedOrigins = ["http://localhost:1234", "http://testsite.com", "http://localhost:8080", "https://myflixapplication-paddy-fac687c8aed3.herokuapp.com"];
+let allowedOrigins = ["http://localhost:1234", "http://testsite.com", "http://localhost:8080"];
 
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -42,19 +42,7 @@ app.use(cors({
 }));
 
 // Handle preflight requests
-app.options('*', cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  },
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true
-}));
+app.options('*', cors());
 
 // Welcome message
 app.get('/', (req, res) => {
