85c7357bc863d24de3a174493d03f95e09e3b572
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-06-30 01:09:06 +0200

    updated auth.js

--- auth.js
+++ auth.js
@@ -1,12 +1,12 @@
 const jwtSecret = 'your_jwt_secret'; // Replace with your actual JWT secret
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+/* const LocalStrategy = require('passport-local').Strategy; */
 const Models = require('./models.js');
 const passportJWT = require('passport-jwt');
 
 const Users = Models.User;
-const JWTStrategy = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
+/* const JWTStrategy = passportJWT.Strategy; */
+/* const ExtractJWT = passportJWT.ExtractJwt; */
 
 passport.use(new LocalStrategy(
   {
@@ -33,38 +33,48 @@ passport.use(new LocalStrategy(
   }
 ));
 
-passport.use(new JWTStrategy(
-  {
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: jwtSecret
-  },
-  async (jwtPayload, callback) => {
-    try {
-      const user = await Users.findById(jwtPayload._id);
-      if (!user) {
-        return callback(null, false, { message: 'User not found' });
-      }
-      return callback(null, user);
-    } catch (error) {
-      return callback(error);
-    }
+// Allows to authenticate users based on the JWT submitted alongside their request.
+var JwtStrategy = require('passport-jwt').Strategy,
+ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = jwtSecret;
+passport.use(new JwtStrategy(opts, async function (jwt_payload, done) {
+try {
+  console.log(jwt_payload, 'jwt_payload')
+  const user = await Users.findById(jwt_payload.id)
+  if (user) {
+    return done(null, user)
+  } else {
+    return done(null, false)
   }
-));
+} catch (error) {
+  done(error)
+}
+}));
 
 module.exports = function (app) {
-  app.post('/login', (req, res) => {
+  app.post('/login', (req, res, next) => {
+    console.log(req.body, 'login');
+    next()
+  }, (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
+        console.log('Authentication failed:', error || info);
         return res.status(400).json({
-          message: 'Something is not right',
-          user: user
+          message: 'Invalid credentials'
         });
       }
+
       req.login(user, { session: false }, (error) => {
         if (error) {
           res.send(error);
         }
-        const token = jwt.sign(user.toJSON(), jwtSecret, { expiresIn: '1h' });
+
+        const token = jwt.sign({ username: user.Username, id: user.id }, jwtSecret, {
+          expiresIn: '7d'
+        });
+
         return res.json({ user, token });
       });
     })(req, res);
