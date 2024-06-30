34ecd795d92cbda8ea2fa45ccc83899aaf3c321d
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-06-30 14:33:26 +0200

    updated auth.js

--- auth.js
+++ auth.js
@@ -2,11 +2,8 @@ const jwtSecret = 'your_jwt_secret'; // Replace with your actual JWT secret
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const Models = require('./models.js');
-/* const passportJWT = require('passport-jwt'); */
 
 const Users = Models.User;
-/* const JWTStrategy = passportJWT.Strategy; */
-/* const ExtractJWT = passportJWT.ExtractJwt; */
 
 passport.use(new LocalStrategy(
   {
@@ -14,49 +11,53 @@ passport.use(new LocalStrategy(
     passwordField: 'Password'
   },
   (username, password, callback) => {
-    console.log(username + ' ' + password);
+    console.log(`Attempting to authenticate user: ${username}`);
     Users.findOne({ Username: username })
       .then((user) => {
         if (!user) {
+          console.log('User not found');
           return callback(null, false, { message: 'Incorrect username or password.' });
         }
 
-        user.validatePassword(password, (err, isMatch) => {
-          if (err) { return callback(err); }
-          if (!isMatch) {
-            return callback(null, false, { message: 'Incorrect username or password.' });
-          }
-          return callback(null, user);
-        });
+        if (!user.isValidPassword(password)) {
+          console.log('Password does not match');
+          return callback(null, false, { message: 'Incorrect username or password.' });
+        }
+        
+        console.log('Authentication successful');
+        return callback(null, user);
       })
-      .catch((err) => { return callback(err); });
+      .catch((err) => {
+        console.log('Error finding user:', err);
+        return callback(err);
+      });
   }
 ));
 
-// Allows to authenticate users based on the JWT submitted alongside their request.
 var JwtStrategy = require('passport-jwt').Strategy,
-ExtractJwt = require('passport-jwt').ExtractJwt;
+    ExtractJwt = require('passport-jwt').ExtractJwt;
 var opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = jwtSecret;
+
 passport.use(new JwtStrategy(opts, async function (jwt_payload, done) {
-try {
-  console.log(jwt_payload, 'jwt_payload')
-  const user = await Users.findById(jwt_payload.id)
-  if (user) {
-    return done(null, user)
-  } else {
-    return done(null, false)
+  try {
+    console.log('JWT payload:', jwt_payload);
+    const user = await Users.findById(jwt_payload.id);
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (error) {
+    done(error);
   }
-} catch (error) {
-  done(error)
-}
 }));
 
 module.exports = function (app) {
   app.post('/login', (req, res, next) => {
-    console.log(req.body, 'login');
-    next()
+    console.log('Login request body:', req.body);
+    next();
   }, (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
@@ -71,7 +72,7 @@ module.exports = function (app) {
           res.send(error);
         }
 
-        const token = jwt.sign({ username: user.Username, id: user.id }, jwtSecret, {
+        const token = jwt.sign({ username: user.Username, id: user._id }, jwtSecret, {
           expiresIn: '7d'
         });
 
