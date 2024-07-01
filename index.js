4989351f7b4d29c2fa7de524c392b92f59ef7560
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-07-02 00:07:33 +0200

    updated index.js

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
 
+// CORS setup
+const cors = require('cors');
+app.use(cors());
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 // Passport configuration
 app.use(passport.initialize());
 require('./auth')(app);
@@ -30,24 +47,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride());
 
-// CORS setup
-app.use(cors({
-  origin: 'http://localhost:1234', // Update this to your frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
 
-app.post('/login', (req, res) => {
-  // Handle login logic
-  res.header('Access-Control-Allow-Origin', 'http://localhost:1234');
-  res.header('Access-Control-Allow-Credentials', 'true');
-    // Other response handling
-    res.json({ message: 'Login successful' });
-  });
-
-// Handle preflight requests
-app.options('*', cors());
 
 // Welcome message
 app.get('/', (req, res) => {
