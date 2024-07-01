9114672e9231b8528583a4862d17af08411c3344
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-07-01 20:30:20 +0200

    auth

--- auth.js
+++ auth.js
@@ -77,11 +77,6 @@ module.exports = function (app) {
           expiresIn: '7d'
         });
 
-        res.header('Access-Control-Allow-Credentials', 'true');
-        res.header('Access-Control-Allow-Origin', 'http://localhost:1234');
-        res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-
         return res.json({ user, token });
       });
     })(req, res);
