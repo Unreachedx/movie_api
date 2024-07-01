ec2f5d77a85ce8cff90ccdf2e34c21542f11ee7d
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-07-01 20:55:11 +0200

    updated auth.js

--- auth.js
+++ auth.js
@@ -1,71 +1,45 @@
-const jwtSecret = process.env.JWT_SECRET; // Use environment variable for JWT secret
+const jwtSecret = process.env.JWT_SECRET;
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const Models = require('./models.js');
 const jwt = require('jsonwebtoken');
+const Models = require('./models.js');
 
 const Users = Models.User;
 
+// Configure Local Strategy for Passport
 passport.use(new LocalStrategy(
   {
     usernameField: 'Username',
     passwordField: 'Password'
   },
   (username, password, callback) => {
-    console.log(`Attempting to authenticate user: ${username}`);
     Users.findOne({ Username: username })
       .then((user) => {
         if (!user) {
-          console.log('User not found');
           return callback(null, false, { message: 'Incorrect username or password.' });
         }
 
         if (!user.isValidPassword(password)) {
-          console.log('Password does not match');
           return callback(null, false, { message: 'Incorrect username or password.' });
         }
 
-        console.log('Authentication successful');
         return callback(null, user);
       })
-      .catch((err) => {
-        console.log('Error finding user:', err);
-        return callback(err);
-      });
+      .catch((err) => callback(err));
   }
 ));
 
-const JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
-let opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = jwtSecret;
-
-passport.use(new JwtStrategy(opts, async function (jwt_payload, done) {
-  try {
-    console.log('JWT payload:', jwt_payload);
-    const user = await Users.findById(jwt_payload.id);
-    if (user) {
-      return done(null, user);
-    } else {
-      return done(null, false);
-    }
-  } catch (error) {
-    done(error);
-  }
-}));
+// Function to generate JWT token
+const generateToken = (user) => {
+  return jwt.sign({ username: user.Username, id: user._id }, jwtSecret, { expiresIn: '7d' });
+};
 
 module.exports = function (app) {
+  // Handle POST request to /login
   app.post('/login', (req, res, next) => {
-    console.log('Login request body:', req.body);
-    next();
-  }, (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
-        console.log('Authentication failed:', error || info);
-        return res.status(400).json({
-          message: 'Invalid credentials'
-        });
+        return res.status(400).json({ message: 'Invalid credentials' });
       }
 
       req.login(user, { session: false }, (error) => {
@@ -73,12 +47,10 @@ module.exports = function (app) {
           res.send(error);
         }
 
-        const token = jwt.sign({ username: user.Username, id: user._id }, jwtSecret, {
-          expiresIn: '7d'
-        });
+        const token = generateToken(user);
 
         return res.json({ user, token });
       });
-    })(req, res);
+    })(req, res, next);
   });
 };
